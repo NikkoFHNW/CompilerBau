cc1effce711ad3a4fa5fbaf96ed94c6a37a0f73c
Nikko Purba <nikko.purba@students.fhnw.ch>
2017-01-15 01:02:41 +0100

    finished the expr context checkers except exprFuncCall

--- Compiler/src/ch/fhnw/compiler/parser/abs/ExprDyadic.java
+++ Compiler/src/ch/fhnw/compiler/parser/abs/ExprDyadic.java
@@ -1,7 +1,9 @@
 package ch.fhnw.compiler.parser.abs;
 
 import ch.fhnw.compiler.scanner.data.Operator;
+import ch.fhnw.compiler.scanner.data.Terminal;
 import ch.fhnw.compiler.scanner.data.TokenTupel;
+import ch.fhnw.compiler.scanner.data.Type;
 import ch.fhnw.lederer.virtualmachineFS2015.ICodeArray;
 
 public class ExprDyadic implements IAbs.IExpr {
@@ -17,12 +19,85 @@ public class ExprDyadic implements IAbs.IExpr {
 
     @Override
     public TokenTupel checkR() throws ContextError {
-        return null;
+        TokenTupel e1 = expr1.checkL();
+        TokenTupel e2 = expr2.checkR();
+        TokenTupel res;
+        
+        switch(operator.getOp()){
+        case PLUS:
+        case MINUS:
+        case TIMES:
+        case DIV:
+        case MOD:
+            if (e1.getType()==Type.INT32 
+            && e2.getType() == Type.INT32) {
+            res= new TokenTupel(Terminal.TYPE, Type.INT32); 
+            return res;
+        } else {
+            throw new ContextError(
+                    "Type error in Operator "
+                    + operator.getOp(),
+                    operator.getLineNr());
+        }
+        case EQ:
+        case NE:
+            if (e1.getType() == Type.BOOL
+                && e2.getType() == Type.BOOL) {
+            	res = new TokenTupel(Terminal.TYPE, Type.BOOL);
+                return res;
+            }else {
+                throw new ContextError(
+                        "Type error in Operator "
+                        + operator.getOp(),
+                        operator.getLineNr());
+            }
+        case GT:
+        case LT:
+        case GE:
+        case LE:
+            if (e1.getType() == Type.INT32 
+            && e2.getType() == Type.INT32) {
+            	res = new TokenTupel(Terminal.TYPE, Type.BOOL);
+            return res;
+        } else {
+            throw new ContextError(
+                    "Type error in Operator "
+                    + operator.getOp(),
+                    operator.getLineNr());
+        }
+        case CAND:
+        case COR:
+            if (e1.getType() == Type.BOOL
+                && e2.getType() == Type.BOOL) {
+            	res = new TokenTupel(Terminal.TYPE, Type.BOOL);
+                return res;
+            } else {
+                throw new ContextError(
+                        "Type error in Operator "
+                        + operator.getOp(),
+                        operator.getLineNr());
+            }
+        default:  throw new ContextError(
+                "Type error in Operator "
+                + operator.getOp(),
+                operator.getLineNr());
     }
+    }
+        
+        
+        
+    	
+    	
+    	
+    	
 
     @Override
     public TokenTupel checkL(boolean canInit) throws ContextError {
-        return null;
+        throw new ContextError(
+                "Found operator " 
+                + operator.getOp() 
+                + "in the left part of an assignement",
+                operator.getLineNr());
     }
 
     @Override
@@ -43,7 +118,7 @@ public class ExprDyadic implements IAbs.IExpr {
 	@Override
 	public TokenTupel checkL() throws ContextError {
 		// TODO Auto-generated method stub
-		return null;
+		throw new ContextError("should be called as rightexpr", this.expr1.getLine());
 	}
 
 	@Override
--- Compiler/src/ch/fhnw/compiler/parser/abs/ExprStore.java
+++ Compiler/src/ch/fhnw/compiler/parser/abs/ExprStore.java
@@ -1,6 +1,10 @@
 package ch.fhnw.compiler.parser.abs;
 
+import ch.fhnw.compiler.Compiler;
+import ch.fhnw.compiler.context.Store;
+import ch.fhnw.compiler.scanner.data.Terminal;
 import ch.fhnw.compiler.scanner.data.TokenTupel;
+import ch.fhnw.compiler.scanner.data.Type;
 import ch.fhnw.lederer.virtualmachineFS2015.ICodeArray;
 
 public class ExprStore implements IAbs.IExpr {
@@ -14,12 +18,39 @@ public class ExprStore implements IAbs.IExpr {
 
     @Override
     public TokenTupel checkR() throws ContextError {
-        return null;
+    	TokenTupel res=null;
+    	if(isInitialization.equals(IsInitialization.INITIALIZATION)){
+    		throw new ContextError("cannot initialise here", 0);
+    	}
+    	Store sto = Compiler.getScope().getStoreTable().getStore(ident);
+    	if(sto==null)throw new ContextError(ident + " wasn't declared.", 0);
+    	if(!sto.isInitialized())throw new ContextError(ident +"hasn't been initialised yet", 0);
+    	Type t = sto.getType();
+    	
+    	res = new TokenTupel(Terminal.TYPE, t);
+    	
+        return res;
     }
 
     @Override
     public TokenTupel checkL(boolean canInit) throws ContextError {
-        return null;
+    	TokenTupel res=null;
+    	if(isInitialization.equals(IsInitialization.INITIALIZATION)){
+    		Store sto = Compiler.getScope().getStoreTable().getStore(ident);
+        	if(sto==null)throw new ContextError(ident + " wasn't declared.", 0);
+        	sto.initialize();
+        	Type t = sto.getType();
+        	res = new TokenTupel(Terminal.TYPE,t);
+        	return res;
+    	}
+    	else{
+    		Store sto = Compiler.getScope().getStoreTable().getStore(ident);
+        	if(sto==null)throw new ContextError(ident + " wasn't declared.", 0);
+        	if(!sto.isInitialized())throw new ContextError(ident +"hasn't been initialised yet", 0);
+        	Type t = sto.getType();
+        	res = new TokenTupel(Terminal.TYPE,t);
+        	return res;
+    	}
     }
 
     @Override
